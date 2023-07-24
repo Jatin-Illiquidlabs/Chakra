8ae9c80009bbacaa186cb284ebf5f8b21341a3c3
aykutdev1 <aakdag@robustentertainment.com>
2023-07-24 17:22:22 +0300

    EnemyAttack

---
+++ Source/ChakraC/Public/GAS/Abilities/ChakraFistAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Abilities/DamageChakraGameplayAbility.h"
+#include "ChakraFistAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAKRAC_API UChakraFistAttack : public UDamageChakraGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
