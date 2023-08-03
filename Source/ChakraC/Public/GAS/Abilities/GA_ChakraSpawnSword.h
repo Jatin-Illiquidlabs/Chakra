1b59740712da27299170b96788ea33c262d66467
aykutdev1 <aakdag@robustentertainment.com>
2023-08-03 15:45:25 +0300

    Shakti Teleport Ability

---
+++ Source/ChakraC/Public/GAS/Abilities/GA_ChakraSpawnSword.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/ChakraProjectileBase.h"
+#include "GAS/Abilities/DamageChakraGameplayAbility.h"
+#include "GA_ChakraSpawnSword.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAKRAC_API UGA_ChakraSpawnSword : public UDamageChakraGameplayAbility
+{
+	GENERATED_BODY()
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Projectile")
+	void SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch = false, float PitchOverride = 0.f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AChakraProjectileBase> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 NumProjectiles = 5;
+};
